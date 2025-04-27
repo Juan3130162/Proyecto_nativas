1b1ee1baf1f80468d41db3d164a567a58b14b35a
juan- <juan-8as@hotmail.es>
2025-04-27 13:33:42 -0500

    Elements in activity_login.xml

--- app/src/main/java/com/example/proyecto_nativas/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/proyecto_nativas/Activities/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyecto_nativas.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_nativas.R
 
@@ -8,5 +10,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnComenzar: Button = findViewById(R.id.btn_comenzar)
+
+        btnComenzar.setOnClickListener {
+            val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/proyecto_nativas/Activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_nativas.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto_nativas.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
