32105f5ebc5ad9991b46a69c86e7ffbb0842cc1b
Cristhian Sepulveda <90584717+Cris-0001@users.noreply.github.com>
2025-04-27 16:12:33 -0500

    Finalizacion activity_product.xml Creacion ProductActivity, Agregar imagenes en drawable, crear edit_text_rounded.xml, Enlazar boton View cart

---
+++ app/src/main/java/com/example/proyecto_nativas/Activities/ProductActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyecto_nativas.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto_nativas.R
+
+class ProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+
+        val btn_viweCart: Button = findViewById(R.id.btn_viwe_cart)
+
+        btn_viweCart.setOnClickListener {
+            val intent = Intent(this@ProductActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
