d22ef6a3cf7460daa8c0d7b4fae2e16fc38179ad
juan- <juan-8as@hotmail.es>
2025-04-27 16:01:44 -0500

    Create activity_register.xml and activity_recover.xml

--- app/src/main/java/com/example/proyecto_nativas/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/proyecto_nativas/Activities/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyecto_nativas.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_nativas.R
 
@@ -9,5 +11,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val btnSignup: Button = findViewById(R.id.btn_sign_up)
+
+        btnSignup.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnForgotPassword: Button = findViewById(R.id.btn_forgot_password)
+
+        btnForgotPassword.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RecoverActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
